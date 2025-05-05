b734b19d949724c33998288b07395067c13e6e80
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-05-05 22:40:23 +0300

    add Event model with fillable properties and bookings relationship

---
+++ Back-end/app/Models/Event.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'date',
+        'start_time',
+        'end_time',
+        'location',
+        'organizer',
+        'category',
+        'image',
+        'status',
+        'ticket_price',
+        'venue_name',
+    ];
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+}
