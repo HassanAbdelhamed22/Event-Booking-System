bdf64b1a1ec1535046efb9fb4c2ab73436c7bb77
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-05-06 20:31:05 +0300

    refactor EventController to improve event management functionality

--- Back-end/app/Http/Controllers/EventController.php
+++ Back-end/app/Http/Controllers/EventController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    // Get all events
+    public function index()
+    {
+        $events = Event::paginate(10);
+        return response()->json([
+            'events' => $events,
+            'meta' => [
+                'current_page' => $events->currentPage(),
+                'last_page' => $events->lastPage(),
+                'per_page' => $events->perPage(),
+                'total' => $events->total(),
+            ],
+        ], 200);
+    }
+
+    // Get a single event
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+        return response()->json($event, 200);
+    }
+
+    // Search events
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $events = Event::where('name', 'LIKE', "%{$query}%")
+            ->orWhere('description', 'LIKE', "%{$query}%")
+            ->get();
+
+        return response()->json($events, 200);
+    }
+
+    // Filter events by date
+    public function filterByDate(Request $request)
+    {
+        $date = $request->input('date');
+        $events = Event::whereDate('date', $date)->get();
+
+        return response()->json($events, 200);
+    }
+
+    // Filter events by location
+    public function filterByLocation(Request $request)
+    {
+        $location = $request->input('location');
+        $events = Event::where('location', 'LIKE', "%{$location}%")->get();
+
+        return response()->json($events, 200);
+    }
+
+    // Filter events by category    
+    public function filterByCategory(Request $request)
+    {
+        $category = $request->input('category');
+        $events = Event::where('category', 'LIKE', "%{$category}%")->get();
+
+        return response()->json($events, 200);
+    }
 }
