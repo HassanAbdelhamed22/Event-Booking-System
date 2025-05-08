77412bd0e71b5d208afb938aacd8f3be69d1d4d4
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-05-08 11:02:56 +0300

    add authentication service with login, register, sign out, and get current user functions

---
+++ Frontend/src/services/auth.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { BASE_URL } from "../constant";
+import api from "./api";
+import type { LoginCredentials, RegisterCredentials } from "../types";
+
+export const login = async (credentials: LoginCredentials) => {
+  const { data, status } = await axios.post(
+    `${BASE_URL}auth/login`,
+    credentials,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return { data, status };
+};
+
+export const register = async (credentials: RegisterCredentials) => {
+  const { data, status } = await axios.post(
+    `${BASE_URL}auth/register`,
+    credentials,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return { data, status };
+};
+
+export const signOut = async () => {
+  const { data, status } = await api.post(`${BASE_URL}auth/logout`, null, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return { data, status };
+};
+
+export const getCurrentUser = async () => {
+  const { data, status } = await api.post(`${BASE_URL}user`, null, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return { data, status };
+};
