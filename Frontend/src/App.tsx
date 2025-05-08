97f5df32afc372dcc5c783b61119b2fce1b9a9aa
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-05-08 09:09:25 +0300

    refactor: clean up App.tsx formatting and remove unused App.css import

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
+    <div className="flex flex-column items-center justify-center gap-2">
+      <div className="flex flex-row items-center justify-center gap-2">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +27,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
