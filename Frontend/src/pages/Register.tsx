c4708cc0d8d9c15bb70262f71bc8dc831e10740a
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-05-08 17:39:03 +0300

    implement Register page layout with Header and RegisterForm

--- Frontend/src/pages/Register.tsx
+++ Frontend/src/pages/Register.tsx
@@ -1,8 +1,20 @@
+import RegisterForm from "../components/forms/RegisterForm";
+import Header from "../layouts/Header";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="min-h-screen flex flex-col">
+      <Header />
 
-export default Register
+      <main className="flex-grow py-12 bg-gray-50">
+        <div className="container-custom">
+          <div className="max-w-2xl mx-auto">
+            <RegisterForm />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Register;
