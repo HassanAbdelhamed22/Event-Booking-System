7adf7fa13b78bf7412ac82d4dfc9f638334e57c3
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-05-11 16:36:29 +0300

    add Booking component for admin page

---
+++ Frontend/src/pages/admin/Booking.tsx
@@ -0,0 +1,14 @@
+import Header from "../../layouts/Header";
+import Footer from "../../layouts/Footer";
+
+const Booking = () => {
+  return (
+    <div>
+      <Header />
+      Booking
+      <Footer />
+    </div>
+  );
+};
+
+export default Booking;
